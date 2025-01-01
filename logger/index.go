2ece9ad14bcb92d35f5d14a04bd1ba2a80941706
disfell <gooday.alan@foxmail.com>
2025-01-01 15:13:32 +0800

    logger no file

--- logger/index.go
+++ logger/index.go
@@ -11,10 +11,10 @@ var zL *zap.Logger
 
 func init() {
 	// 创建文件写入器
-	file, err := os.OpenFile("app.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
-	if err != nil {
-		panic(err)
-	}
+	// file, err := os.OpenFile("app.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	// if err != nil {
+	//	panic(err)
+	// }
 	// 初始化 Zap 日志
 	encoderConfig := zapcore.EncoderConfig{
 		MessageKey:     "msg",
@@ -33,8 +33,8 @@ func init() {
 	zL = zap.New(zapcore.NewCore(
 		// zapcore.NewJSONEncoder(encoderConfig), // JOSN 格式输出日志
 		zapcore.NewConsoleEncoder(encoderConfig), // 常规的控制台输出格式
-		// zapcore.AddSync(os.Stdout), // 输出到控制台
-		zapcore.AddSync(file), // 输出到文件
+		zapcore.AddSync(os.Stdout),               // 输出到控制台
+		// zapcore.AddSync(file), // 输出到文件
 		zap.NewAtomicLevelAt(zapcore.DebugLevel),
 	))
 }
